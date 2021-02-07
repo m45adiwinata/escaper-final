4f08773983332e377bce0eb2a8a56153a634dc32
m45adiwinata <m45adiwinata@gmail.com>
2021-02-07 14:39:29 +0800

    contact fix 2

--- resources/views/emailcontact.blade.php
+++ resources/views/emailcontact.blade.php
@@ -1,14 +1,14 @@
-<h1>Comment from {{$temp['name']}}</h1>
+<h1>Comment from {{$name}}</h1>
 <table>
     <tr>
         <td>email</td>
         <td>:</td>
-        <td>{{$temp['email']}}</td>
+        <td>{{$email}}</td>
     </tr>
     <tr>
         <td>phone</td>
         <td>:</td>
-        <td>{{$temp['phone']}}</td>
+        <td>{{$phone}}</td>
     </tr>
 </table>
-<p>{{$temp->comment}}</p>
+<p>{{$comment}}</p>
