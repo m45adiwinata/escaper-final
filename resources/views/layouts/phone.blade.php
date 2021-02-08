8cd2d550606d799f9843a10684048d3c93ee1e6d
m45adiwinata <m45adiwinata@gmail.com>
2021-02-09 01:10:24 +0800

    google analytics

--- resources/views/layouts/phone.blade.php
+++ resources/views/layouts/phone.blade.php
@@ -20,6 +20,15 @@
             font-family: escaperfont;
         }
     </style>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-30D3EE0RS0"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-30D3EE0RS0');
+    </script>
     </head>
     <body>
         @yield('content')
